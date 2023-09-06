1c556bbb57e87f6004d9f5b580022c2c0d0c4402
hariprasanth23 <harifbi175@gmail.com>
2023-09-06 11:35:42 +0530

    Spring quick start code added

---
+++ SpringBoot-Projects/springbootbasics/_1_quickstart/src/main/java/com/learn/springboot/basicapplication/BasicapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.learn.springboot.basicapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicapplicationApplication.class, args);
+	}
+	// check application.properties once
+}
---
+++ SpringBoot-Projects/springbootbasics/_1_quickstart/src/test/java/com/learn/springboot/basicapplication/BasicapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.springboot.basicapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
